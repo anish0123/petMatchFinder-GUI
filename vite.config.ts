8313604745b294e0b13bc49a6e8a08a947d31146
Anish Maharjan <anishmhn@hotmail.com>
2024-05-09 00:53:15 +0300

    add: deploy workflow for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/petMatchFinder-GUI/",
   server: {    
     // this ensures that the browser opens upon server start
     open: true,
